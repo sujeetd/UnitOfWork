0c2f22f5473a9407914c81d0c572e45a29b640e4
sujeetd <sujeetd@gmail.com>
2016-05-31 13:26:22 -0500

    Create IUserRepository.cs

---
+++ User/IUserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Your.Business
+{
+    public interface IUserRepository : IRepository2<User, int>
+    {
+        //bool ValidateUserFromCC(User entity);
+    }    
+}
