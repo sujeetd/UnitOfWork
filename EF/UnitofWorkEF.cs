a6cfe5b8bd7bc82560e6bc27ba94226b923545e3
sujeetd <sujeetd@gmail.com>
2016-06-09 08:52:20 -0500

    Update UnitofWorkEF.cs

--- EF/UnitofWorkEF.cs
+++ EF/UnitofWorkEF.cs
@@ -10,43 +10,54 @@ namespace Your.Business.EF
     {
         protected AppContext _context;
         protected DbContextTransaction _transaction;
+        protected string _connectionalias;
 
         public UnitOfWorkEF(string connectionalias = "default")
         {
-            _context = new AppContext(AppConfigConnectionFactory.GetConnectionString(connectionalias, Settings.New()));
+            _connectionalias = connectionalias;
+            _context = new AppContext(AppConfigConnectionFactory.GetConnectionString(_connectionalias, Settings.New()));
             _context.Configuration.LazyLoadingEnabled = false;
-            //_transaction = _context.Database.BeginTransaction();            
+            _transaction = _context.Database.BeginTransaction();
+
         }
 
         public void BeginTransaction()
         {
 
-            _transaction = _context.Database.BeginTransaction();
+           
         }
 
         public void Commit()
         {
             _context.SaveChanges();
             if (_transaction != null)
+            {
                 _transaction.Commit();
-            
+                //start a new transaction
+                _transaction = _context.Database.BeginTransaction();
+            }
         }
 
         public void Dispose()
         {
+            //if (_transaction != null)
+            //{
+            //    _transaction.Rollback();
+            //    _transaction.Dispose();
+            //}
+            //_transaction = null;
+            //for singleton instance keep context open
             _context.Dispose();
-            if (_transaction != null)
-            {
-                _transaction.Rollback();
-                _transaction.Dispose();
-            }
-            _transaction = null;
         }
 
         public void Rollback()
         {
             if (_transaction != null)
+            {
                 _transaction.Rollback();
+                //start a new transaction
+                _transaction = _context.Database.BeginTransaction();
+            }
         }
     }
 }
